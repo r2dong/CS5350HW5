d170fe45cdb5aa2b83ae2ac0d4e76bee1098de51
r2dong <r2dong@yahoo.com>
2018-04-07 20:12:11 -0500

    improve style

--- src/main/TransformationSequence.java
+++ src/main/TransformationSequence.java
@@ -8,14 +8,15 @@ public class TransformationSequence {
 	HashMap<Triple, ArrayList<String>> mem;
 	
 	// handles initializations for the algorithm
-	public ArrayList<String> getTranSeqWrapper(final String in, final String target, int start) {
+	public ArrayList<String> getTranSeqWrapper(String in, String t) {
 		mem = new HashMap<>();
-		return getTranSeq(in, target, start);
+		return getSeq(in, t, 0);
 	}
 	
-	public ArrayList<String> getTranSeq(String in, final String target, int start) {
+	// the actual algorithm
+	private ArrayList<String> getSeq(String in, String t, int start) {
 		
-		Triple key = new Triple(in, target, false);
+		Triple key = new Triple(in, t, false);
 		
 		// return answer if subproblem already solved
 		if (mem.containsKey(key))
@@ -33,69 +34,84 @@ public class TransformationSequence {
 		 * deal
 		 */
 		if (inLength == 1) {
-			if (in.charAt(0) == target.charAt(0))
+			if (in.charAt(0) == t.charAt(0))
 				return new ArrayList<String>();
 			else {
-				ArrayList<String> toReturn = new ArrayList<>();
-				toReturn.add("substitute letter \"" + in.charAt(0) + "\" at index " + start + " with \"" + target.charAt(0) + "\"");
-				return toReturn;
+				ArrayList<String> answer = new ArrayList<>();
+				answer.add(makeSubStr(in.charAt(0), start, t.charAt(0)));
+				return answer;
 			}
 		}
 		
 		
 		ArrayList<String> sumArr;
 		
-		// recursive case 1
+		// recursive case 1, try all possible ways of splitting
 		ArrayList<String> curMinFlipArr = null;
-		ArrayList<String> arr1;
-		ArrayList<String> arr2;
-		ArrayList<String> arr3;
+		ArrayList<String> a1;
+		ArrayList<String> a2;
+		ArrayList<String> a3;
 		for (int i = 0; i <= inLength; i++)
-			for (int j = i + 2; j <= inLength; j++) {				
-				arr1 = getTranSeq(new String(in.substring(0, i)), target.substring(0, i), start);
-				arr2 = getNoFlipSeq(new String(in.substring(i, j)), target.substring(i, j), start + i);
-				arr3 = getTranSeq(new String(in.substring(j, inLength)), target.substring(j, inLength), start + j);
+			for (int j = i + 2; j <= inLength; j++) {
+				a1 = getSeq(in.substring(0, i), t.substring(0, i), start);
+				a2 = getNFSeq(in.substring(i, j), t.substring(i, j), start + i);
+				a3 = getSeq(in.substring(j, inLength), 
+						t.substring(j, inLength), start + j);
 				sumArr = new ArrayList<>();
-				sumArr.addAll(arr1);
-				sumArr.addAll(arr2);
-				sumArr.addAll(arr3);
-				sumArr.add("flip(" + (i + start) + ", " + (j + start - 1) + ")");
+				sumArr.addAll(a1);
+				sumArr.addAll(a2);
+				sumArr.addAll(a3);
+				sumArr.add(makeFlipStr(i + start, j + start - 1));
 				if (curMinFlipArr == null)
 					curMinFlipArr = sumArr;
 				else
-					curMinFlipArr = sumArr.size() < curMinFlipArr.size() ? sumArr : curMinFlipArr;
+					curMinFlipArr = sumArr.size() < curMinFlipArr.size() ? 
+							sumArr : curMinFlipArr;
 			}
-		// recursive case 2
+		// recursive case 2, try all possible ways of substitution
 		ArrayList<String> curMinSubArr = null;
 		ArrayList<String> arr5;
 		ArrayList<String> arr6;
 		for (int i = 0; i < inLength; i++) {
-			if (in.charAt(i) != target.charAt(i)) { 
-				arr5 = getTranSeq(new String(in.substring(0, i)), target.substring(0, i), start);
-				arr6 = getTranSeq(new String(in.substring(i + 1, inLength)), target.substring(i + 1, inLength), start + i + 1);
+			if (in.charAt(i) != t.charAt(i)) { 
+				arr5 = getSeq(in.substring(0, i), t.substring(0, i), start);
+				arr6 = getSeq(in.substring(i + 1, inLength), 
+						t.substring(i + 1, inLength), start + i + 1);
 				sumArr = new ArrayList<>();
 				sumArr.addAll(arr5);
 				sumArr.addAll(arr6);
-				sumArr.add("substitute letter \"" + in.charAt(i) + "\" at index " + (i + start) + " with \"" + target.charAt(i) + "\"");
+				sumArr.add(makeSubStr(in.charAt(i), i + start, t.charAt(i)));
 				if (curMinSubArr == null)
 					curMinSubArr = sumArr;
 				else
-					curMinSubArr = sumArr.size() < curMinSubArr.size() ? sumArr : curMinSubArr;
+					curMinSubArr = sumArr.size() < curMinSubArr.size() ? 
+							sumArr : curMinSubArr;
 			}
 		}
-		// handle edge case: curMinSubArr not initialized due to already correct array
+		// in case curMinSubArr not initialized for already correct array
 		if (curMinSubArr == null)
 			curMinSubArr = new ArrayList<String>();
 		
 		// update memorization table, than return
-		ArrayList<String> answer = curMinFlipArr.size() < curMinSubArr.size() ? curMinFlipArr : curMinSubArr;
+		ArrayList<String> answer = curMinFlipArr.size() < curMinSubArr.size() ?
+				curMinFlipArr : curMinSubArr;
 		mem.put(key, answer);
 		return answer;
 	}
 	
-	private ArrayList<String> getNoFlipSeq(String in, final String target, int start) {
+	private String makeFlipStr(int start, int end) {
+		return "flip(" + start + ", " + end + ")";
+	}
+	
+	private String makeSubStr(char source, int index, char t) {
+		return "substitute letter \"" + source + "\" at index " + index + 
+				" with \"" + t + "\"";
+	}
+	
+	// calculate transformation path when a subarray can no loger be flipped
+	private ArrayList<String> getNFSeq(String in, final String t, int start) {
 		
-		Triple key = new Triple(in, target, true);
+		Triple key = new Triple(in, t, true);
 		
 		// return answer if sub-problem already solved
 		if (mem.containsKey(key))
@@ -103,23 +119,16 @@ public class TransformationSequence {
 		
 		ArrayList<String> seq = new ArrayList<String>();
 		int inLength = in.length();
-		int reversedIndex;
+		int revInd;
 		// check in reverse order due to the flip
 		for (int i = 0; i < inLength; i++) {
-			reversedIndex = inLength - 1 - i;
-			if (in.charAt(i) != target.charAt(reversedIndex))
-				seq.add("substitute letter \"" + in.charAt(i) + "\" at index " + (reversedIndex + start) + " with \"" + target.charAt(reversedIndex) + "\"");
+			revInd = inLength - 1 - i;
+			if (in.charAt(i) != t.charAt(revInd))
+				seq.add(makeSubStr(in.charAt(i), revInd + start, 
+						t.charAt(revInd)));
 		}
 		mem.put(key, seq);
 		return seq;
-	}
-	
-	private String reverse(String in) {
-		String toReturn = "";
-		int inLength = in.length();
-		for (int i = 0; i < inLength; i++)
-			toReturn += in.charAt(inLength - 1 - i);
-		return toReturn;
 	}
 	
 	public void reverselyPrintArray(ArrayList<String> arr) {
@@ -131,25 +140,9 @@ public class TransformationSequence {
 		TransformationSequence t = new TransformationSequence();
 		String source;
 		String target;
-		//source = "timefliezlijeanbrrow";
-		//target = "timeflieslikeanarrow";
-		//source = "lijb";
-		//target = "lika";
-		//source = "abc";
-		//target = "def";
-		// source = "ab";
-		// target = "de";
-		//source = "abcf";
-		//target = "baec";
-		//source = "abcdy";
-		//target = "xbzdf";
-		//source = "lijfdsafdsafsd";
-		//target = "lijfdsafdsafsd";
-		//source = "li";
-		//target = "li";
 		source = "timeflieslikeanarrow";
 		target = "tfemiliilzejeworrbna";
 		
-		t.reverselyPrintArray(t.getTranSeqWrapper(source, target, 0));
+		t.reverselyPrintArray(t.getTranSeqWrapper(source, target));
 	}
 }
